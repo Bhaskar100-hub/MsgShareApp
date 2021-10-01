363cda8dfaa3e0b1dd6382e459666f57c8cbb762
Bhaskar100-hub <bhaskarwary7@gmail.com>
2021-10-01 23:47:14 +0530

    first commit after the end of 2nd part of the tutorial

---
+++ app/src/main/java/com/example/waryshareapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.waryshareapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnShowToast.setOnClickListener {
+            Toast.makeText(this, "Button is Click", Toast.LENGTH_SHORT ).show()
+        }
+        btnPassMsg.setOnClickListener {
+            val message: String = txtUserMessage.text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("user_message", message)
+            startActivity(intent)
+        }
+        btnShareToOtherApps.setOnClickListener {
+            val message: String = txtUserMessage.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Share to : "))
+        }
+    }
+}
